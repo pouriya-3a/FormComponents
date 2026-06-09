029fa9fc75dec736e8300c2da66e4d7cb19e9fb3
pouriya-3a <pooryasaboori1385@gmail.com>
2026-06-09 19:12:57 +0330

    import export

---
+++ FormComponents/index.jsx
@@ -0,0 +1,15 @@
+import CustomArrayTextField from "./CustomArrayTextField";
+import CustomAutocomplete from "./CustomAutoComplete";
+import CustomFormButtons from "./CustomFormButtons";
+import CustomPersianDatePicker from "./CustomPersianDatePicker";
+import CustomTextField from "./CustomTextField";
+import LoadingModal from "./LoadingModal";
+
+export {
+  CustomArrayTextField,
+  CustomAutocomplete,
+  CustomFormButtons,
+  CustomPersianDatePicker,
+  CustomTextField,
+  LoadingModal,
+};
